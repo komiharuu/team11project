faa3d66d574830b2d00bee0ef2701d8b20639010
soobeen0301 <xg12041@gmail.com>
2024-06-03 19:43:08 +0900

    Feat : 프로필 수정 API 기능 구현

--- src/middlewares/access-token.middleware.js
+++ src/middlewares/access-token.middleware.js
@@ -1,26 +1,25 @@
-
-import jwt from 'jsonwebtoken';
-import { prisma } from '../utils/prisma.util.js';
+import jwt from "jsonwebtoken";
+import { prisma } from "../utils/prisma.util.js";
 const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY;
 
 export default async function (req, res, next) {
   try {
     const { authorization } = req.cookies;
-    if (!authorization) throw new Error('토큰이 존재하지 않습니다.');
+    if (!authorization) throw new Error("토큰이 존재하지 않습니다.");
 
-    const [tokenType, accessToken] = authorization.split(' ');
+    const [tokenType, accessToken] = authorization.split(" ");
 
-    if (tokenType !== 'Bearer')
-      throw new Error('토큰 타입이 일치하지 않습니다.');
+    if (tokenType !== "Bearer")
+      throw new Error("토큰 타입이 일치하지 않습니다.");
 
     const decodedToken = jwt.verify(accessToken, ACCESS_TOKEN_SECRET_KEY);
     const userId = decodedToken.userId;
 
     const user = await prisma.user.findFirst({
-      where: { userId: +userId },
+      where: { userId: userId },
     });
     if (!user) {
-      throw new Error('토큰 사용자가 존재하지 않습니다.');
+      throw new Error("토큰 사용자가 존재하지 않습니다.");
     }
 
     // req.user에 사용자 정보를 저장합니다.
@@ -28,17 +27,16 @@ export default async function (req, res, next) {
 
     next();
   } catch (error) {
-
     // 토큰이 만료되었거나, 조작되었을 때, 에러 메시지를 다르게 출력합니다.
     switch (error.name) {
-      case 'TokenExpiredError':
-        return res.status(401).json({ message: '토큰이 만료되었습니다.' });
-      case 'JsonWebTokenError':
-        return res.status(401).json({ message: '토큰이 조작되었습니다.' });
+      case "TokenExpiredError":
+        return res.status(401).json({ message: "토큰이 만료되었습니다." });
+      case "JsonWebTokenError":
+        return res.status(401).json({ message: "토큰이 조작되었습니다." });
       default:
         return res
           .status(401)
-          .json({ message: error.message ?? '비정상적인 요청입니다.' });
+          .json({ message: error.message ?? "비정상적인 요청입니다." });
     }
   }
 }
